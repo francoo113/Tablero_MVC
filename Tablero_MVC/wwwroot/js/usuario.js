ec99de495eade454348c5a2982311290f7d02167
FDC248150 <fc_9902@outlook.com>
2023-11-16 14:41:17 -0300

    agrego validación mail en create de usuario + f(x)es en hoja nueva de .js

---
+++ Tablero_MVC/wwwroot/js/usuario.js
@@ -0,0 +1,19 @@
+﻿function validateEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+
+$(document).ready(function () {
+    $("#mailInput").on("input", function () {
+        var email = $(this).val();
+
+        if (validateEmail(email)) {
+            $("#emailError").text("");
+        } else {
+            $("#emailError").text("Por favor ingresa un correo electrónico válido");
+        }
+    });
+});
+
+
